c0ab93dc1fe4351e6204850067484e35175ebf25
Timo Sand <timo.sand@kapsi.fi>
2009-11-13 15:40:13 +0200

    Refined root-route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :courses
-  map.index '/', :controller => :courses, :action => :index
+  map.root :controller => :courses, :action => :index
   map.connect ':controller/:action/:id'
  # map.connect ':controller/:action/:id.:format'
 end
