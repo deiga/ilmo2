065b32d2d87cc3caa936d6491c272daadd4b74f6
Timo Sand <timo.sand@kapsi.fi>
2009-11-13 15:37:22 +0200

    Added functionality to the controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,17 +1,34 @@
 class UserController < ApplicationController
   def register
+    @user = User.new(@params[:user])
+    if request.post?  
+      if @user.save
+        session[:user] = User.authenticate(@user.username, @user.password)
+        flash[:message] = "Registering successful"
+        redirect_to :action => "welcome"          
+      else
+        flash[:warning] = "Registering unsuccessful"
+      end
+    end
   end
 
   def login
+    if request.post?
+      if session[:user] = User.authenticate(params[:user][:username], params[:user][:password])
+        flash[:message]  = "Login successful"
+        redirect_to_stored
+      else
+        flash[:warning] = "Login unsuccessful"
+      end
+    end
   end
 
   def logout
   end
-
-  def delete
+  
+  def welcome
   end
-
-  def edit
+  def hidden
   end
 
 end
