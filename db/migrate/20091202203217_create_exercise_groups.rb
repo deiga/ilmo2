d221d33e8f24310699096ba0b8f23b282928027f
Timo Sand <timo.sand@kapsi.fi>
2009-12-02 22:34:47 +0200

    created models and controllers for exercise groups

---
+++ db/migrate/20091202203217_create_exercise_groups.rb
@@ -0,0 +1,16 @@
+class CreateExerciseGroups < ActiveRecord::Migration
+  def self.up
+    create_table :exercise_groups do |t|
+      t.belongs_to :course_instance
+      t.string :weekday
+      t.string :time_of_day
+      t.string :class
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :exercise_groups
+  end
+end
