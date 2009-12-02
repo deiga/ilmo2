ada59bda81efd78b470839876d3c1e71a5d4ee84
Timo Sand <timo.sand@kapsi.fi>
2009-12-02 22:02:55 +0200

    Modified routes and instance's show for groups

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ ActionController::Routing::Routes.draw do |map|
   map.logout '/sessions/logout', :controller => "sessions", :action => "logout", :conditions => { :method => :get }
 
   map.resources :courses do |courses|
-    courses.resources :instances, :controller => 'course_instances' #do |cis|
-      #cis.resources :groups, :controller => 'exercise_groups'
-    #end
+    courses.resources :instances, :controller => 'course_instances' do |cis|
+      cis.resources :groups, :controller => 'exercise_groups'
+    end
   end
   map.resources :users, :except => [:index]
   map.resources :sessions, :only => [:index, :create, :destroy]
