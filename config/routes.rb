c1a08c658cbd6ef27541c44b14feb3ff19367ce3
Timo Sand <timo.sand@kapsi.fi>
2009-12-03 16:06:53 +0200

    Added admin tool. Started working on user registration to exercise groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,26 @@
 ActionController::Routing::Routes.draw do |map|
 
+  map.resources :users, :except => [:index]
+  map.resources :sessions, :only => [:index, :create, :destroy]
+  
   map.resources :courses, :shallow => true do |course|
     course.resources :course_instances do |ci|
       ci.resources :exercise_groups
     end
   end
-  map.resources :users, :except => [:index]
-  map.resources :sessions, :only => [:index, :create, :destroy]
   
-  map.root :controller => :courses
+  map.namespace(:admin) do |admin|
+    admin.resources :users, :active_scaffold => true
+    admin.resources :courses, :active_scaffold => true
+    admin.resources :course_instances, :active_scaffold => true
+    admin.resources :exercise_groups, :active_scaffold => true
+  end
+  
+  map.resource :admin, :controller => :admin  
+  
   map.login "/login", :controller=>:sessions
   map.logout "/logout", :controller=>:sessions, :action=>:destroy
+  
+  map.root :controller => :courses
  
- # map.connect ':controller/:action/:id'
- # map.connect ':controller/:action/:id.:format'
 end
