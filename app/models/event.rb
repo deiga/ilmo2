b21972e037876ba52d72ef798788c6fcadee9262
Timo Sand <timo.sand@kapsi.fi>
2009-12-08 20:15:34 +0200

    Deleted Events, staying with old system

--- app/models/event.rb
+++
@@ -1,7 +0,0 @@
-class Event < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :course
-  
-  default_scope :order => "updated_at DESC"
-  named_scope :current_events, :order => "created_at DESC", :include => [:user, :course]
-end
