40d794cb2c416bf1da4fdd55c4d85e2bb06f68ec
Timo Sand <timo.sand@kapsi.fi>
2009-12-08 14:56:35 +0200

    finished events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,9 +2,9 @@ module EventsHelper
   
   def event_text(event)
     case event[:type]
-    when 'NewCourseEvent'       then ""
-    when 'NewUserEvent'         then ""
-    when 'NewRegistrationEvent' then ""
+    when 'NewCourseEvent'       then "#{link_to h(event.course.title), course_url(event.course)} has been added to the course catalog!"
+    when 'NewUserEvent'         then "#{h(event.user.username)} has joined ilmo2.0!"
+    when 'NewRegistrationEvent' then "#{h(event.user.username)} is attending #{link_to h(event.course.title), course_url(event.course)}"
     end
   end
 end
