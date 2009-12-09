5e7196c1af79d71b0662592ad2702bf4301f1d82
Timo Sand <timo.sand@kapsi.fi>
2009-12-09 15:33:59 +0200

    Created course category model

---
+++ spec/models/course_category_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe CourseCategory do
+  before(:each) do
+    @valid_attributes = {
+      :course_id => 1,
+      :name => "value for name"
+    }
+  end
+
+  it "should create a new instance given valid attributes" do
+    CourseCategory.create!(@valid_attributes)
+  end
+end
