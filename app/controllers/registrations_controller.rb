a25983936e6546bb68c4711c180613a2470662d8
Timo Sand <timo.sand@kapsi.fi>
2009-12-09 14:33:56 +0200

    Showing and deleting registrations works

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,8 +1,18 @@
 class RegistrationsController < ApplicationController
   
   def create
-    Registration.create :user_id => current_user, :exercise_group_id => params[:exercise_group_id]
+    reg = Registration.create :user_id => current_user, :exercise_group_id => params[:exercise_group_id]
+    if reg.errors.any?
+      reg.errors.full_messages.each do |msg|
+        flash[:notice] = msg
+      end
+    end
     redirect_to exercise_group_url(params[:exercise_group_id])
   end
   
+  def destroy
+    reg = Registration.find_by_user_id_and_exercise_group_id(params[:user_id], params[:group_id])
+    reg.delete
+    redirect_to edit_user_url(current_user)
+  end
 end
